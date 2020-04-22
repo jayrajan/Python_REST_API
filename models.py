40dfe3b94e608fbfb2df5a28f3ce4d04221acc86
Jerin Rajan <jrajan@jprtech.co.uk>
2020-04-22 13:21:35 +0100

    added comments to code

--- models.py
+++ models.py
@@ -1,12 +1,18 @@
 # Code written by Jerin Rajan on 15th April 2020
-# models.py - handles everything that involves a Database.
+# models.py - handles data and business logic (Model)
+
 class calculation:
+    # Int which specifies the range size
     size = 10000001
+    # initialising the list of numbers to perform the sum operation
     numbers_to_add = []
 
     def __init__(self):
+        # generating the list of numbers based on the input int size
         self.numbers_to_add = list(range(self.size))
-
+# The function which performs the sum operation
     def total(self):
+        # performing the sum operation
         response = str(sum(self.numbers_to_add))
+        # outputting the response
         return response
